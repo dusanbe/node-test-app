654b0f0f1ad3258f174222cc7283725cf1f5abce
Dusan Biocanin <dusanbiocanin@hotmail.com>
2018-08-01 16:45:13 +0200

    beta

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const hbs = require('hbs');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const momentData = require('./moment');
 
 var port = process.env.PORT || 3000;
@@ -12,9 +12,12 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 //default values
-var moment = momentData.moment(2.05, 1.719);
+var MB = "MB30";
+var fb = getfb(MB);
+var coeffK = 1.719;
 
 var layout = {
+		title: 'Bending moment',
     xaxis: {
       title: 'Section width [cm]',
       titlefont: {
@@ -33,22 +36,92 @@ var layout = {
     }
   };
 
-var plotlyScript = `<script> Plotly.newPlot('chart', [{ z: ${moment}, type: 'surface' }], ${JSON.stringify(layout)}); </script>`;
+function getPlotlyScript (fb, coeffK) {
+	return `<script> Plotly.newPlot('chart', [{ z: ${momentData.moment(fb, coeffK)}, type: 'surface' }], ${JSON.stringify(layout)}); </script>`;
+}
+
+function getfb (MB) {
+  	let fb;
+  	switch (MB){
+    	case "MB30":
+      	fb = 2.05;
+      	break;
+   	case "MB35":
+      	fb = 2.35;
+      	break;
+   	case "MB40":
+      	fb = 2.55;
+      	break;
+   	case "MB45":
+      	fb = 2.85;
+			break;
+		case "MB50":
+      	fb = 3.0;
+			break;
+		default:
+			fb = 2.05;
+	}
+	return fb;
+}
 
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('htmlPlotGen', () => {
-    return plotlyScript;
+    return getPlotlyScript(fb, coeffK);
+});
+
+hbs.registerHelper('showCoeffK', () => {
+	return coeffK;
+});
+
+hbs.registerHelper('showMB30', () => {
+	if(MB == "MB30"){
+		return "selected";
+	} else{
+		return "";
+	}
+});
+
+hbs.registerHelper('showMB35', () => {
+	if(MB == "MB35"){
+		return "selected";
+	} else{
+		return "";
+	}
+});
+
+hbs.registerHelper('showMB40', () => {
+	if(MB == "MB40"){
+		return "selected";
+	} else{
+		return "";
+	}
+});
+
+hbs.registerHelper('showMB45', () => {
+	if(MB == "MB45"){
+		return "selected";
+	} else{
+		return "";
+	}
+});
+
+hbs.registerHelper('showMB50', () => {
+	if(MB == "MB50"){
+		return "selected";
+	} else{
+		return "";
+	}
 });
 
 app.get('/ploting', (req, res) => {
-    res.render('ploting.hbs');
+		res.render('ploting.hbs');
 });
 
 app.post('/ploting', function(req, res) {
-    //let MB = res.body.MB;
-    //let coeffK = res.body.coeffK;
-    var moment = momentData.moment(3, 2.5);
-    res.render('ploting.hbs');
+	MB = req.body.MB;
+	fb = getfb(MB);
+	coeffK = parseFloat(req.body.coeffK);
+  res.render('ploting.hbs');
   });
 
 app.listen(port, () => {
